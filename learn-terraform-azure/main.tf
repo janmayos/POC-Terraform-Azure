0d5f8987b330cfdf098c291c0269f352ebb6640a
janmayos <jangasca5@gmail.com>
2026-03-12 15:37:13 -0600

    Variables

--- learn-terraform-azure/main.tf
+++ learn-terraform-azure/main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 }
 #Grupo de recursos
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
+  name     = var.resource_group_name
   location = "westus2"
 
   tags = {
